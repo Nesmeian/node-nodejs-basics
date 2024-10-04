821923688a72d203c10d19fc1111aa7fa3a37ff6
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-10-04 18:09:37 +0300

    feat:add solution for rename"

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import fs from "fs/promises";
 const rename = async () => {
-    // Write your code here 
+  try {
+    const [wrongFileName, renamedFile] = await Promise.all([
+      fs
+        .stat("./files/wrongFilename.txt")
+        .then(() => true)
+        .catch(() => false),
+
+      fs
+        .stat("./files/properFilename.md")
+        .then(() => true)
+        .catch(() => false),
+    ]);
+    if (!wrongFileName || renamedFile) {
+      throw new Error("FS bot");
+    }
+    fs.rename("./files/wrongFilename.txt", "./files/properFilename.md");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await rename();
