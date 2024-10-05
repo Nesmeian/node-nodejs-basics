41103ec732f9a156ea05c925f43c9f7ad0f5e9b7
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-10-06 01:31:21 +0300

    feat:add solution for streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+import path from "path";
+import { createInterface } from "readline";
+import url from "url";
 const write = async () => {
-    // Write your code here 
+  const currentDir = path.dirname(url.fileURLToPath(import.meta.url));
+  const fileToWritePath = path.join(currentDir, "files", "fileToWrite.txt");
+  const writeStream = fs.createWriteStream(fileToWritePath, "utf-8");
+  process.stdout.write("Hi Friend write something there.\n");
+  const input = createInterface(process.stdin);
+  input.on("line", (message) => writeStream.write(message + "\n"));
 };
 
 await write();
