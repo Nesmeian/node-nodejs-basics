ef02e3b4a76e5ab79037535cceb741fb849b1e3b
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-10-06 21:28:13 +0300

    feat:add solution of compess

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,41 @@
+import zlib from "node:zlib";
+import path from "path";
+import url from "url";
+import { pipeline } from "node:stream";
+import fs from "fs/promises";
+import { createReadStream, createWriteStream } from "node:fs";
+
 const decompress = async () => {
-    // Write your code here 
+  const currentPathDir = path.dirname(url.fileURLToPath(import.meta.url));
+  const compressFilePath = path.join(
+    currentPathDir,
+    "files",
+    "fileToCompress.txt"
+  );
+  const compressFileDestinationPath = path.join(
+    currentPathDir,
+    "files",
+    "archive.gz"
+  );
+  const gunzip = zlib.createGunzip();
+  const compressFileDestination = createReadStream(compressFileDestinationPath);
+  const compressFile = createWriteStream(compressFilePath);
+  const checkCompressFile = await fs
+    .stat(compressFilePath)
+    .then(() => true)
+    .catch(() => false);
+  try {
+    pipeline(compressFileDestination, gunzip, compressFile, (err) => {
+      if (err) {
+        console.log(err);
+        throw new Error("FS operation failed");
+      } else {
+        console.log("Decompressing is completed");
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await decompress();
