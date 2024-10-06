5a407864ebc60637c5a85a22a7e276f9d84419bd
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-10-06 18:53:31 +0300

    feat:add solution of hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import path from "path";
+import { createHash } from "node:crypto";
+import url from "url";
+import fs from "fs";
 const calculateHash = async () => {
-  // Write your code here
+  const currentPathDir = path.dirname(url.fileURLToPath(import.meta.url));
+  const fileToCalculatePath = path.join(
+    currentPathDir,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+  const readStream = fs.createReadStream(fileToCalculatePath, "utf8");
+  const hashFunc = createHash("sha256");
+  readStream.on("data", (chunk) => {
+    hashFunc.update(chunk);
+  });
+  readStream.on("end", () => {
+    const fileHash = hashFunc.digest("hex");
+    console.log(fileHash);
+  });
 };
 
 await calculateHash();
