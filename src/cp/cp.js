a6dfbaeafa4685e45febb953d12447fc404a7ae4
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-10-07 19:00:03 +0300

    feat:add solution to cp add finish task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import url from "url";
+import path from "path";
+import { spawn } from "node:child_process";
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const currentPathDir = path.dirname(url.fileURLToPath(import.meta.url));
+  const scriptPath = path.join(currentPathDir, "files", "script.js");
+  const childProcess = spawn(process.execPath, [scriptPath, ...args], {
+    stdio: ["pipe", "pipe", "inherit"],
+  });
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+
+  return childProcess;
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
